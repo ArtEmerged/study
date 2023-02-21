7b5136a571a99d07de571ba879ab4cc90c355396
aecheist <titan4590it@gmail.com>
2023-02-21 14:09:27 +0600

    4_3Max v.2

--- GrokkingAlg_book/4_3max.go
+++ GrokkingAlg_book/4_3max.go
@@ -6,11 +6,13 @@ func main() {
 
 	array := []int{4, 2, 5, 3, 6, 3, 66, 32, 66, 454, 22}
 
-	fmt.Println(max(array))
+
+	fmt.Println(findMax(array))
+	fmt.Println(findMax2(array))
 
 }
 
-func max(array []int) int {
+func findMax(array []int) int {
 	if len(array) == 1 {
 		return array[0]
 	} else if len(array) == 0 {
@@ -22,9 +24,21 @@ func max(array []int) int {
 		return array[1]
 	}
 
-	maxM := max(array[1:])
+	maxM := findMax(array[1:])
 	if maxM > array[0] {
 		return maxM
 	}
 	return array[0]
+}
+
+
+//version 2
+func findMax2(x []int) int {
+	if len(x) == 0 {
+		return 0
+	}
+	if max := findMax2(x[1:]); max > x[0] {
+		return max
+	}
+	return x[0]
 }
