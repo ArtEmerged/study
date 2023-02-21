4739eb9b47bd22aededcdac130a6a383f8b615c5
aecheist <titan4590it@gmail.com>
2023-02-21 15:54:17 +0600

    add comments

--- Algorithm/AlgEuclidean.go
+++ Algorithm/AlgEuclidean.go
@@ -4,7 +4,9 @@ func main() {
 
 	print(GCD(1680, 640), "\n")
 }
-
+/*  The Euclidean algorithm is a way to find
+the greatest common divisor(GCD) of two positive integers, 
+a and b.*/
 func GCD(x, y int) int {
 	if x%y != 0 {
 		y =GCD(y, x%y)
