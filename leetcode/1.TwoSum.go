01dd30a27bae42d3eb496ad296e83189a827b097
aecheist <titan4590it@gmail.com>
2023-02-22 20:19:35 +0600

    1

---
+++ leetcode/1.TwoSum.go
@@ -0,0 +1,14 @@
+package leet
+
+func twoSum(nums []int, target int) []int {
+    an:=make([]int,2)
+for k, i:= range nums{
+    for j, n:=range nums{
+        if i+n ==target && k != j{
+an[0],an[1]=k,j
+break
+        }
+    }
+}
+return an
+}
