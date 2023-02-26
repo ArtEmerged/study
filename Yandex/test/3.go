12b6038cdfc31a0bbd45b4198e2531fe31481631
aecheist <titan4590it@gmail.com>
2023-02-27 00:09:25 +0600

    v1.1

---
+++ Yandex/test/3.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var quantitySticks, quantityCollector int
+	var N, K string
+	file, _ := os.Open("input.txt")
+	read := bufio.NewScanner(file)
+	for read.Scan() {
+		if quantitySticks == 0 {
+			quantitySticks, _ = strconv.Atoi(read.Text())
+		} else if N == "" {
+			N = read.Text()
+		} else if quantityCollector == 0 {
+			quantityCollector, _ = strconv.Atoi(read.Text())
+		} else {
+			K = read.Text()
+		}
+	}
+	arrN := strings.Split(N, " ")
+	arrK := strings.Split(K, " ")
+	StickersNumber := make([]int, 0, quantitySticks)
+	StickersCollector := make([]int, 0, quantityCollector)
+	for _, s := range arrN {
+		num, err := strconv.Atoi(s)
+		if err == nil {
+			StickersNumber = append(StickersNumber, num)
+		}
+	}
+	for _, s := range arrK {
+		num, err := strconv.Atoi(s)
+		if err == nil {
+			StickersCollector = append(StickersCollector, num)
+		}
+	}
+	// StickersNumber := []int{5}       // 1, 50, 70, 75, 80, 85, 90, 95, 100
+	// StickersCollector := []int{4, 6} //  0  1   2   3  (4)  5   6   7   8
+	sortInt(StickersNumber)
+	for _, number := range StickersCollector {
+		fmt.Println(Search(StickersNumber, number))
+	}
+}
+
+func sortInt(arr []int) []int {
+	if len(arr) > 1 {
+		Left := 0
+		Right := len(arr) - 1
+		middle := arr[(Right+Left)/2]
+		for Left < Right {
+			for arr[Left] < middle {
+				Left++
+			}
+			for arr[Right] > middle {
+				Right--
+			}
+			if Left <= Right {
+				arr[Left], arr[Right] = arr[Right], arr[Left]
+				Left++
+				Right--
+			}
+		}
+		sortInt(arr[:Right+1])
+		sortInt(arr[Left:])
+	}
+	return arr
+
+}
+
+func Search(arr []int, find int) int {
+	if find != 0 {
+		Left := 0
+		Right := len(arr) - 1
+		answer := 0
+		for Left <= Right {
+			middle := (Right + Left) / 2
+			if find == arr[middle] {
+				return middle
+			}
+			if find > arr[middle] {
+				Left = middle + 1
+			} else {
+				Right = middle - 1
+			}
+			answer = middle
+		}
+		if find > arr[answer] {
+			return answer + 1
+		}
+		return answer
+	}
+	return 0
+}
