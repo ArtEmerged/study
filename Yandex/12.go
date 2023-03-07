5c2f5268fb8ea1f28a43e241974df40679098dfb
ArtyomPC <titan4590it@gmail.com>
2023-03-07 23:01:30 +0300

    12

---
+++ Yandex/12.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Stek struct {
+	bracket rune
+	next    *Stek
+}
+
+type List struct {
+	Head *Stek
+}
+
+func main() {
+	l := &List{}
+	var str string
+	fmt.Scanln(&str)
+	for _, v := range str {
+		switch v {
+		case '(':
+			i := &Stek{bracket: v, next: l.Head}
+			l.Head = i
+		case '[':
+			i := &Stek{bracket: v, next: l.Head}
+			l.Head = i
+		case '{':
+			i := &Stek{bracket: v, next: l.Head}
+			l.Head = i
+		case ')':
+			if l.Head == nil || l.Head.bracket != '(' {
+				fmt.Println("no")
+				return
+			} else {
+				l.Head = l.Head.next
+			}
+		case ']':
+			if l.Head == nil || l.Head.bracket != '[' {
+				fmt.Println("no")
+				return
+			} else {
+				l.Head = l.Head.next
+			}
+		default:
+			if l.Head == nil || l.Head.bracket != '{' {
+				fmt.Println("no")
+				return
+			} else {
+				l.Head = l.Head.next
+			}
+		}
+	}
+	if l.Head == nil {
+		fmt.Println("yes")
+	} else {
+		fmt.Println("no")
+	}
+}
