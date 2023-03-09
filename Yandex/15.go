8d357fa9823a27957d31684520a818194e3a950f
ArtyomPC <titan4590it@gmail.com>
2023-03-09 22:42:51 +0300

    15.2

--- Yandex/15.go
+++ Yandex/15.go
@@ -1,52 +1,10 @@
-// package main
-
-// import "fmt"
-
-// // type LIFO struct {
-// // 	Num   int
-// // 	Index int
-// // 	Next  *LIFO
-// // }
-
-// // type List struct {
-// // 	Head *LIFO
-// // }
-
-// func main() {
-// 	lenght := 10
-// 	// fmt.Scanln(&lenght)
-// 	// answer := make([]int, lenght)
-// 	answer2 := []int{1, 2, 3, 2, 1, 4, 2, 5, 3, 1}
-// 	answer := make([]int, lenght)
-// 	arr := make([]int, 0, lenght)
-// 	index := 0
-// 	for i, n := range answer2 {
-// 		// n := 0
-// 		// fmt.Scan(&n)
-// 		for len(arr) != 0 && n < arr[len(arr)-1] {
-// 			index--
-// 			answer[index] = i
-// 			arr = arr[:len(arr)-1]
-// 		}
-// 		arr = append(arr, n)
-// 		index++
-// 	}
-
-// 	for _, v := range answer {
-// 		if v == 0 {
-// 			fmt.Print(-1)
-// 		} else {
-// 			fmt.Print(v)
-// 		}
-// 		fmt.Print(" ")
-// 	}
-// }
-
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"time"
+	"log"
+	"os"
 )
 
 type LIFO struct {
@@ -56,14 +14,19 @@ type LIFO struct {
 }
 
 func main() {
-	t0 := time.Now()
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	in := bufio.NewReader(f)
 	lenght := 0
-	fmt.Scanln(&lenght)
+	fmt.Fscan(in, &lenght)
 	arr := make([]int, lenght)
 	l := &LIFO{}
 	for i := range arr {
 		n := 0
-		fmt.Scan(&n)
+		fmt.Fscan(in, &n)
 		for l != nil && n < l.Num {
 			arr[l.Index] = i
 			l = l.Next
@@ -71,6 +34,17 @@ func main() {
 		push := &LIFO{Num: n, Index: i, Next: l}
 		l = push
 	}
+	// file, err := os.Create("output.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer file.Close()
+
+	// out := bufio.NewWriter(file)
+	// for _, v := range arr {
+	// 	fmt.Fprintf(out, "%d ", v)
+	// }
+
 	for _, v := range arr {
 		if v == 0 {
 			fmt.Print(-1, " ")
@@ -78,6 +52,5 @@ func main() {
 			fmt.Print(v, " ")
 		}
 	}
-	t1 := time.Now()
-	fmt.Printf("\nElapsed time: %v\n", t1.Sub(t0))
+
 }
