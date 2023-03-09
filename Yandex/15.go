7e02c236cb90fc8202c1696dd793c3bdf707ca41
ArtyomPC <titan4590it@gmail.com>
2023-03-09 20:21:46 +0300

    15.1

--- Yandex/15.go
+++ Yandex/15.go
@@ -1,6 +1,53 @@
+// package main
+
+// import "fmt"
+
+// // type LIFO struct {
+// // 	Num   int
+// // 	Index int
+// // 	Next  *LIFO
+// // }
+
+// // type List struct {
+// // 	Head *LIFO
+// // }
+
+// func main() {
+// 	lenght := 10
+// 	// fmt.Scanln(&lenght)
+// 	// answer := make([]int, lenght)
+// 	answer2 := []int{1, 2, 3, 2, 1, 4, 2, 5, 3, 1}
+// 	answer := make([]int, lenght)
+// 	arr := make([]int, 0, lenght)
+// 	index := 0
+// 	for i, n := range answer2 {
+// 		// n := 0
+// 		// fmt.Scan(&n)
+// 		for len(arr) != 0 && n < arr[len(arr)-1] {
+// 			index--
+// 			answer[index] = i
+// 			arr = arr[:len(arr)-1]
+// 		}
+// 		arr = append(arr, n)
+// 		index++
+// 	}
+
+// 	for _, v := range answer {
+// 		if v == 0 {
+// 			fmt.Print(-1)
+// 		} else {
+// 			fmt.Print(v)
+// 		}
+// 		fmt.Print(" ")
+// 	}
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type LIFO struct {
 	Num   int
@@ -8,32 +55,29 @@ type LIFO struct {
 	Next  *LIFO
 }
 
-type List struct {
-	Head *LIFO
-}
-
 func main() {
+	t0 := time.Now()
 	lenght := 0
 	fmt.Scanln(&lenght)
 	arr := make([]int, lenght)
-	l := &List{}
+	l := &LIFO{}
 	for i := range arr {
 		n := 0
 		fmt.Scan(&n)
-		for l.Head != nil && n < l.Head.Num {
-			arr[l.Head.Index] = i
-			l.Head = l.Head.Next
+		for l != nil && n < l.Num {
+			arr[l.Index] = i
+			l = l.Next
 		}
-		add := &LIFO{Num: n, Index: i, Next: l.Head}
-		l.Head = add
+		push := &LIFO{Num: n, Index: i, Next: l}
+		l = push
 	}
-	
 	for _, v := range arr {
 		if v == 0 {
-			fmt.Print(-1)
+			fmt.Print(-1, " ")
 		} else {
-			fmt.Print(v)
+			fmt.Print(v, " ")
 		}
-		fmt.Print(" ")
 	}
+	t1 := time.Now()
+	fmt.Printf("\nElapsed time: %v\n", t1.Sub(t0))
 }
