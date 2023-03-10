2aab6df845a33f3e81e855050bf899f7161226de
ArtyomPC <titan4590it@gmail.com>
2023-03-10 19:34:35 +0300

    19v2

--- Yandex/19.go
+++ Yandex/19.go
@@ -22,34 +22,40 @@ func main() {
 		fmt.Fscanln(in, &step, &num)
 		switch step {
 		case 0: // add element
-			arr = append(arr, num)
-			last := len(arr) - 1
-			dad := (last - 1) / 2
-			for last > 0 && arr[last] > arr[dad] {
-				arr[last], arr[dad] = arr[dad], arr[last]
-				last = dad
-				dad = (last - 1) / 2
-			}
+			add(&arr, num)
 		case 1: //delete element
-			last := len(arr) - 1
 			fmt.Println(arr[0])
-			i := 0
-			arr[i] = arr[last]
-			for 2*i+2 < len(arr) {
-				max := 2*i + 1
-				if arr[max] < arr[2*i+2] {
-					max = 2*i + 2
-				}
-				if arr[i] < arr[max] {
-					arr[i], arr[max] = arr[max], arr[i]
-					i = max
-				} else {
-					break
-				}
-			}
-			arr = arr[:last]
-
+			delete(&arr)
 		}
 	}
+}
 
+func add(arr *[]int, num int) {
+	*arr = append(*arr, num)
+	last := len(*arr) - 1
+	dad := (last - 1) / 2
+	for last > 0 && (*arr)[last] > (*arr)[dad] {
+		(*arr)[last], (*arr)[dad] = (*arr)[dad], (*arr)[last]
+		last = dad
+		dad = (last - 1) / 2
+	}
+}
+
+func delete(arr *[]int) {
+	last := len(*arr) - 1
+	i := 0
+	(*arr)[i] = (*arr)[last]
+	for 2*i+2 < len(*arr) {
+		max := 2*i + 1
+		if (*arr)[max] < (*arr)[2*i+2] {
+			max = 2*i + 2
+		}
+		if (*arr)[i] < (*arr)[max] {
+			(*arr)[i], (*arr)[max] = (*arr)[max], (*arr)[i]
+			i = max
+		} else {
+			break
+		}
+	}
+	*arr = (*arr)[:last]
 }
