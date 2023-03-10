52c91807eab7b2fa38864ad2a33a2a1d8b214cc7
ArtyomPC <titan4590it@gmail.com>
2023-03-10 19:20:40 +0300

    19

---
+++ Yandex/19.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	in := bufio.NewReader(f)
+	lenght := 0
+	fmt.Fscanln(in, &lenght)
+	arr := make([]int, 0, lenght)
+	for z := 0; z < lenght; z++ {
+		step, num := 0, 0
+		fmt.Fscanln(in, &step, &num)
+		switch step {
+		case 0: // add element
+			arr = append(arr, num)
+			last := len(arr) - 1
+			dad := (last - 1) / 2
+			for last > 0 && arr[last] > arr[dad] {
+				arr[last], arr[dad] = arr[dad], arr[last]
+				last = dad
+				dad = (last - 1) / 2
+			}
+		case 1: //delete element
+			last := len(arr) - 1
+			fmt.Println(arr[0])
+			i := 0
+			arr[i] = arr[last]
+			for 2*i+2 < len(arr) {
+				max := 2*i + 1
+				if arr[max] < arr[2*i+2] {
+					max = 2*i + 2
+				}
+				if arr[i] < arr[max] {
+					arr[i], arr[max] = arr[max], arr[i]
+					i = max
+				} else {
+					break
+				}
+			}
+			arr = arr[:last]
+
+		}
+	}
+
+}
