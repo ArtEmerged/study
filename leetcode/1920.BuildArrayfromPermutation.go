87d711f0f959b7398291e6f49cb10ccfd80a1981
aecheist <titan4590it@gmail.com>
2023-04-01 22:22:41 +0300

    1920

---
+++ leetcode/1920.BuildArrayfromPermutation.go
@@ -0,0 +1,12 @@
+package leet
+
+func buildArray(nums []int) []int {
+	n := len(nums)
+	for i := range nums {
+		nums[i] += n * (nums[nums[i]] % n)
+	}
+	for i := range nums {
+		nums[i] /= n
+	}
+	return nums
+}
