edb686926293965fed8701f81d69f28f0c17b98b
aecheist <lilapank@yandex.ru>
2023-09-26 17:26:21 +0600

    ex17

---
+++ test/course_go/ex17_understanding-net-http-package/ParseForm/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type hotdog int
+
+func (h hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tpl.Execute(w, r.Form)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.html"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
